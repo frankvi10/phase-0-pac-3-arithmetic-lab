82db58f17e9be5845f0c51ad42c0fdba98895704
francisco villa salmeron <frankvi@yahoo.com>
2022-03-13 19:08:04 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(){}
+function subtract(){}
+function multiply(){}
+function divide(){}
+
+function add(a, b){
+	return a + b
+}
+
+function subtract(a, b){
+	return a - b
+}
+
+function multiply(a, b){
+	return a * b
+}
+
+function divide(a, b){
+	return a / b
+}
+
+function increment(n) {
+	return ++n
+}
+
+function decrement(n){
+	return --n
+}
+ 
+function makeInt(n){
+	return parseInt
+}
+
+function  markeInt(){
+	return parseInt
+}
+
